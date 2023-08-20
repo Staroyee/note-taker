80b0064a4dcbd5b58b5627e4456a13816db80a08
Staroyee <danmasefield@gmail.com>
2023-08-20 11:04:35 +0930

    Created api routes files and added content to htmlRoutes.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const path = require('path');
+
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const PORT = 3001;
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
